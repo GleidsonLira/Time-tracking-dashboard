48b2380124f5901bfb5966b314fe28d2ced10285
Gleidson Lira <lira.spfc@gmail.com>
2023-03-04 21:49:48 -0300

    Iniciando construção código Javascript

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+let lastTime = document.querySelectorAll(".last"),
+currentTime = document.querySelectorAll(".hrs-current"),
+time = document.querySelectorAll(".time"),
+prev = document.querySelectorAll(".prev")
