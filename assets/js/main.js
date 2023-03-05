4e6c430a0e3680b62a2b18455d79e3f429f45f46
Gleidson Lira <lira.spfc@gmail.com>
2023-03-05 11:12:18 -0300

    Novas adições ao código Javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,16 @@ let lastTime = document.querySelectorAll(".last"),
 currentTime = document.querySelectorAll(".hrs-current"),
 time = document.querySelectorAll(".time"),
 prev = document.querySelectorAll(".prev")
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null); 
+}
